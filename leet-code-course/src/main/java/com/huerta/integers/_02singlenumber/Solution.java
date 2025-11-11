85b6077c81628b27df6e61a0d618accc92236060
Israel Huerta Fer <israelhf24@gmail.com>
2025-11-11 17:50:42 -0600

    Single number

---
+++ leet-code-course/src/main/java/com/huerta/integers/_02singlenumber/Solution.java
@@ -0,0 +1,30 @@
+package com.huerta.integers._02singlenumber;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+  private static int singleNumber(int[] nums) {
+    if (nums.length == 1) {
+      return nums[0];
+    }
+    Set<Integer> set = new HashSet<>();
+    for (int i = 0; i < nums.length; i++) {
+      if (set.contains(i)) {
+        set.remove(i);
+      } else {
+        set.add(nums[i]);
+      }
+    }
+    return set.iterator().next();
+  }
+
+  public static int singleNumberImproved(int[] nums) {
+    int singleNumber = 0;
+    for (int i : nums) {
+      singleNumber ^= i;
+    }
+    return singleNumber;
+  }
+}
